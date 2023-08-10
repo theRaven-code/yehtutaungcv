c08df7edc31141de12d9a42c20d3e545fffe501f
Sam <yehtutaung@abank.com.mm>
2023-08-10 11:25:31 +0630

    amendment

--- src/App.tsx
+++ src/App.tsx
@@ -153,7 +153,7 @@ function App() {
             })}
           </ol>
         </section>
-        <a href="mailto:email@example.com">
+        <a href="mailto:isaravananofficial@gmail.com">
           <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
               Contact Me
