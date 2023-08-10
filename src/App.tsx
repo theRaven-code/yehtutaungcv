aa0d0c3bb531dbdaf7f8f2650a07cb2ebec369bd
theRaven-code <83681754+theRaven-code@users.noreply.github.com>
2023-08-10 13:29:51 +0630

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -160,7 +160,7 @@ function App() {
             </span>
           </button>
         </a>
-        <a href="https://1drv.ms/b/s!AnPXTB_olAFRgfECooD2Y6O9MmNeuA?e=1D7hOj" rel="noopener noreferrer">
+        <a href="https://1drv.ms/b/s!AnPXTB_olAFRgfECooD2Y6O9MmNeuA?e=PLnSX8" rel="noopener noreferrer" target="_blank">
           <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
               Download CV
